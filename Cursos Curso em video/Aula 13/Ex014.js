4e7f7a04ff17549f9128476beb9e39b0df4d6eeb
Tafarel Mello <tafarel.luis.mello@gmail.com>
2022-11-21 22:44:33 -0300

    commit
    Feat: exercice interactiviti

---
+++ Cursos Curso em video/Aula 13/Ex014.js
@@ -0,0 +1,25 @@
+function carregar(){
+    var msg = document.getElementById('msg')
+    var img = document.getElementById('imagem')
+    var data = new Date()
+    var hora = data.getHours()
+    msg.innerHTML = `Agora sao ${hora} horas.`
+    if(hora >= 0 && hora < 12){
+        // Bom dia
+        img.src = 'imagens/manha.jpg'
+        document.body.style.background = '#e2cd9f'
+
+    } else if(hora >= 12 && hora < 18){
+        // Boa tarde
+        img.src = 'imagens/tarde.jpg'
+        document.body.style.background = '#b9846f'
+
+    } else{
+        // Boa noite
+        img.src = 'imagens/noite.jpg'
+        document.body.style.background = '#515154'
+
+    }
+}
+
+
