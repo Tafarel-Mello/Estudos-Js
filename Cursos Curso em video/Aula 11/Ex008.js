392c7fef170a7934170552666bb9ae3a51cfc482
Tafarel Mello <tafarel.luis.mello@gmail.com>
2022-11-21 21:01:58 -0300

    Commit
    Feat: start learning conditions in Js

---
+++ Cursos Curso em video/Aula 11/Ex008.js
@@ -0,0 +1,6 @@
+var vel = 50
+console.log(`A velocidade do seu carro eh de ${vel} km/h`)
+if(vel > 60){ // condicao simples
+    console.log(`Voce ultrapassou a velocidade permitida! MULTADO!`)
+}
+console.log(`Dirija sempre de cinto!`)
---
+++ Cursos Curso em video/Aula 11/Ex009.js
@@ -0,0 +1,7 @@
+var pais = 'Brasil'
+console.log(`Se voce nasceu em ${pais}`)
+if(pais != 'Brasil'){
+    console.log('Voce eh estrangeiro!')
+} else{
+    console.log('Voce eh brasileiro!')
+}
