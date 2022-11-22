204c05a9682e8da03ff54a3cc6c11818f3b1b126
Tafarel Mello <tafarel.luis.mello@gmail.com>
2022-11-21 21:31:24 -0300

    commit
    Feat: il-else-if

---
+++ Cursos Curso em video/Aula 12/Ex011.js
@@ -0,0 +1,9 @@
+var idade = 14
+if(idade < 16)
+{
+    console.log('Nao pode votar!')
+} else if(idade < 18){
+        console.log('Seu voto eh opcional')
+} else if(idade > 18){
+            console.log('Seu voto eh obrigatorio!')
+}
