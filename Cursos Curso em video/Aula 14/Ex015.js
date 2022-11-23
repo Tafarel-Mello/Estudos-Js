4916765471b3f5c8a450c5bc1d15efba85d9eff3
Tafarel Mello <tafarel.luis.mello@gmail.com>
2022-11-22 22:34:12 -0300

    commit
    Feat: exercices

--- Cursos Curso em video/Aula 14/Ex015.js
+++ Cursos Curso em video/Aula 14/Ex015.js
@@ -9,6 +9,45 @@ function verificar(){
     } else{
         var fsex = document.getElementsByName('radsex')
         var idade = ano - Number(fAno.value)
-        res.innerHTML = `Idade calculada: ${idade} anos.`
+        var genero = ''
+
+        var img = document.createElement('img') // criar elemento no HTML dinamicamente
+        img.setAttribute('id', 'foto') // atribui o id ao elemento
+
+        if(fsex[0].checked){ // teste do botao selecionado
+            genero = 'Homen'
+            if(idade >= 0 && idade < 10){
+                // crianca
+                img.setAttribute('src', 'imagens/bebe_menino.jpg')
+            } else if(idade < 22){
+                // jovem
+                img.setAttribute('src', 'imagens/jovem_homem.jpg')
+            } else if(idade < 50){
+                // adulto
+                img.setAttribute('src', 'imagens/adulto_homem.jpg')
+            } else {
+                // idoso
+                img.setAttribute('src', 'imagens/idoso_homem.jpg')
+            }
+        } else if(fsex[1].checked){ // teste do botao selecionado
+            genero = 'Mulher'
+            if(idade >= 0 && idade < 10){
+                // crianca
+                img.setAttribute('src', 'imagens/bebe_menina.jpg')
+            } else if(idade < 22){
+                // jovem
+                img.setAttribute('src', 'imagens/jovem_mulher.jpg')
+            } else if(idade < 50){
+                // adulto
+                img.setAttribute('src', 'imagens/adulto_mulher.jpg')
+            } else {
+                // idoso
+                img.setAttribute('src', 'imagens/idoso_mulher.jpg')
+            }
+
+        }
+        res.style.textAlign = 'center' // propriedade CSS via Js
+        res.innerHTML = `Detectamos ${genero} de ${idade} anos.`
+        res.appendChild(img) // inserir a imagem no HTML via Js
     }
 }
