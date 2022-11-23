2615de871840be644a70339fed732a536a42ef07
Tafarel Mello <tafarel.luis.mello@gmail.com>
2022-11-22 22:10:42 -0300

    commit
    Feat: Js exercices

---
+++ Cursos Curso em video/Aula 14/Ex015.js
@@ -0,0 +1,14 @@
+function verificar(){
+    var data = new Date()
+    var ano = data.getFullYear()
+    var fAno = document.getElementById('txtano')
+    var res = document.getElementById('res')
+
+    if(fAno.value.length == 0 || Number(fAno.value) > ano){
+        window.alert('[ERRO] Verifique os dados e tente novamente!')
+    } else{
+        var fsex = document.getElementsByName('radsex')
+        var idade = ano - Number(fAno.value)
+        res.innerHTML = `Idade calculada: ${idade} anos.`
+    }
+}
