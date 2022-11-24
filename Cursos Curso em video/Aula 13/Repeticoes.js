77810590c99e72aacbb3e3339a7401ffdb33e5f2
Tafarel Mello <tafarel.luis.mello@gmail.com>
2022-11-23 21:11:11 -0300

    commit
    feat: start learning repetitions in Js

---
+++ Cursos Curso em video/Aula 13/Repeticoes.js
@@ -0,0 +1,13 @@
+// repeticoes com WHILE
+var c = 1
+while(c <= 6){
+    console.log('Tudo bem?')
+    c++
+}
+
+// repeticao com DO WHILE
+var d = 1
+do{
+    console.log('Sim, tudo bem!')
+    d++
+} while (d <= 6)
