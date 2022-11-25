72cb3b37350b94080469d97bf2e8dbd4e2a81e87
Tafarel Mello <tafarel.luis.mello@gmail.com>
2022-11-25 19:19:39 -0300

    commit
    Feat: keep learning arrays

--- Cursos Curso em video/Aula 15/ambiente aula 15.js
+++ Cursos Curso em video/Aula 15/ambiente aula 15.js
@@ -1,5 +1,6 @@
-let num = [10, 5, 2, 44 ,1 ,7]
+let num = [2, 5, 8, 7, 6, 4, 3, 1, 9]
 num.sort() // ordena de forma crescente o vetor
+
 /*
 num.push(10)      // adicionar na ultima posicao do vetor
 num.sort()        // ordena de forma crescente o vetor
@@ -17,3 +18,14 @@ for(let pos = 0 ; pos < num.length ; pos++){
 for(let pos in num){
     console.log(`A posicao ${pos} contem o valor ${num[pos]}`)
 }
+
+let x = 5
+let procura = num.indexOf(x) // procura a posicao do valor inscrito na variavel
+
+if(procura == -1){ // -1 eh sempre a representacao para posicao inexistente
+    console.log('Valor nao encontrado!')
+} else{
+    console.log(`O valor ${x} esta na posicao ${procura} do vetor`)
+}
+
+
