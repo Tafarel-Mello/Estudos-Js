2e8ea5186f38d49d40b463a7bbcf1667a90f4253
Tafarel Mello <tafarel.luis.mello@gmail.com>
2022-12-03 14:28:28 -0300

    commit
    Feat: App Calculadora

--- Cursos Udemy/Calculadora/Repositorie Calculadora/script.js
+++ Cursos Udemy/Calculadora/Repositorie Calculadora/script.js
@@ -1,3 +1,21 @@
 function calcular(tipo, valor){
-    console.log(tipo, valor)
+    
+    if(tipo === 'acao'){
+        if(valor === 'c'){ // logica do clear
+            document.getElementById('resultado').value = ''
+        }
+
+        if(valor === '+' || valor === '-' || valor === '/' || valor === '*' || valor === '.'){
+            document.getElementById('resultado').value += valor
+        }
+        if(valor === '='){
+            let valor_campo = eval(document.getElementById('resultado').value) // comando eval transforma a string em uma expressao Js para o interpretador sem necessidade de transformar
+            document.getElementById('resultado').value = valor_campo
+        }
+
+    }else if(tipo === 'valor'){
+        // let valor_campo = document.getElementById('resultado').value -> recuperando valor e usando variavel
+        // document.getElementById('resultado').value = valor_campo + valor
+        document.getElementById('resultado').value += valor // ja atribui sem a necessidade de recuperar antes
+    }
 }
